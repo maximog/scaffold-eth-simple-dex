c724cebde441b6c9e7b0446b0cf8e3627e12a3aa
Maximo Gijon <maximo.gijon@gmail.com>
2022-01-16 22:53:39 -0300

    implement functionality for DEX contract

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -1,16 +1,81 @@
 pragma solidity >=0.8.0 <0.9.0;
-// SPDX-License-Identifier: MIT
-// import "hardhat/console.sol";
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
+    IERC20 token;
+    uint256 public totalLiquidity;
+    mapping(address => uint256) public liquidity;
 
-  IERC20 token;
+    constructor(address token_addr) {
+        token = IERC20(token_addr);
+    }
 
-  constructor(address token_addr) {
-    token = IERC20(token_addr);
-  }
+    function init(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0, "DEX already has liquidity");
+        totalLiquidity = address(this).balance;
+        liquidity[msg.sender] = totalLiquidity;
+        require(token.transferFrom(msg.sender, address(this), tokens));
+        return totalLiquidity;
+    }
 
-  // write your functions here...
+    function price(
+        uint256 input_amount,
+        uint256 input_reserve,
+        uint256 output_reserve
+    ) public pure returns (uint256) {
+        uint256 input_amount_with_fee = input_amount * 997;
+        uint256 numerator = input_amount_with_fee * output_reserve;
+        uint256 denominator = input_reserve * 1000 + input_amount_with_fee;
+        return numerator / denominator;
+    }
 
+    function ethToToken() public payable returns (uint256) {
+        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 tokens_bought = price(
+            msg.value,
+            address(this).balance - msg.value,
+            token_reserve
+        );
+        require(token.transfer(msg.sender, tokens_bought));
+        return tokens_bought;
+    }
+
+    function tokenToEth(uint256 tokens) public returns (uint256) {
+        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 eth_bought = price(
+            tokens,
+            token_reserve,
+            address(this).balance
+        );
+        (bool success, ) = msg.sender.call{value: eth_bought}("");
+        require(success);
+        require(token.transferFrom(msg.sender, address(this), tokens));
+        return eth_bought;
+    }
+
+    function deposit() public payable returns (uint256) {
+        uint256 eth_reserve = address(this).balance - msg.value;
+        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 token_amount = ((msg.value * token_reserve) / eth_reserve) + 1;
+        uint256 liquidity_minted = (msg.value * totalLiquidity) / eth_reserve;
+        liquidity[msg.sender] += liquidity_minted;
+        totalLiquidity += liquidity_minted;
+        require(token.transferFrom(msg.sender, address(this), token_amount));
+        return liquidity_minted;
+    }
+
+    function withdraw(uint256 amount) public returns (uint256, uint256) {
+        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 eth_amount = (amount * address(this).balance) / totalLiquidity;
+        uint256 token_amount = (amount * token_reserve) / totalLiquidity;
+        liquidity[msg.sender] -= eth_amount;
+        totalLiquidity -= eth_amount;
+        payable(msg.sender).transfer(eth_amount);
+        require(token.transfer(msg.sender, token_amount));
+        return (eth_amount, token_amount);
+    }
 }
